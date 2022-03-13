ffd8b7b3cdd58853898f4713341f534a00154650
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-03-13 11:32:57 +0100

    Renaming PlayerMumbleClient as PlayerMumbleServer

---
+++ src/main/java/fr/pederobien/mumble/client/impl/PlayerMumbleServer.java
@@ -0,0 +1,30 @@
+package fr.pederobien.mumble.client.impl;
+
+import java.net.InetSocketAddress;
+import java.util.function.Consumer;
+
+import fr.pederobien.mumble.client.interfaces.IMumbleServerPlayer;
+import fr.pederobien.mumble.client.interfaces.IResponse;
+
+public class PlayerMumbleServer extends MumbleServer implements IMumbleServerPlayer {
+
+	/**
+	 * Creates a client associated to a specific player.
+	 * 
+	 * @param name    The server name name.
+	 * @param address The server address.
+	 */
+	public PlayerMumbleServer(String name, InetSocketAddress address) {
+		super(name, address);
+	}
+
+	@Override
+	public void join(Consumer<IResponse> callback) {
+
+	}
+
+	@Override
+	public void leave(Consumer<IResponse> callback) {
+
+	}
+}
