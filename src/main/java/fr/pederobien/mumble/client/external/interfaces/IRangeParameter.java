3d03af92692bd10a9a52495c428d94b5be73d8ad
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-05-14 19:27:16 +0200

    Deleting method getSoundModifier from interface IRangeParameter

--- src/main/java/fr/pederobien/mumble/client/external/interfaces/IRangeParameter.java
+++ src/main/java/fr/pederobien/mumble/client/external/interfaces/IRangeParameter.java
@@ -5,11 +5,6 @@ import fr.pederobien.mumble.client.common.interfaces.ICommonRangeParameter;
 public interface IRangeParameter<T> extends ICommonRangeParameter<T>, IParameter<T> {
 
 	/**
-	 * @return The sound modifier associated to this parameter.
-	 */
-	ISoundModifier getSoundModifier();
-
-	/**
 	 * Clone this parameter. It creates a new parameter based on the properties of this parameter.
 	 * 
 	 * @return A new parameter.
